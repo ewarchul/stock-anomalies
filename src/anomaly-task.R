9321d8e81c4a9b31436e0d8e82ad1ffb8fee6831
warbarbye <fryd.wtf@gmail.com>
2020-05-25 15:37:11 +0200

    Add function to creating task object from config file

--- src/anomaly-task.R
+++ src/anomaly-task.R
@@ -1,9 +1,11 @@
 library(tidyverse)
+library(yaml)
 library(magrittr)
 library(R6)
 library(mlr)
 library(here)
 source(here::here("src", "utils-task.R"))
+source(here::here("src", "data-preparation.R"))
 
 
 #' @title Anomaly Detection Task
@@ -46,49 +48,23 @@ AnomalyTask = R6::R6Class("AnomalyTask",
     )
 )
 
-prepare_experiment_data = function(config_path) {
-  config = read_yaml(config_path)
-  print(config)
-  if (config$experiment_point_anomalies$data_source == "wow"){
-    wow_data = prepare_WoWToken(config$experiment_point_anomalies$data_path)
-    data = wow_data %>%
-      dplyr::filter(region == "eu") %>%
-      generate_df(key = time)
-  }else{
-    wig_data = prepare_Stock(config$experiment_point_anomalies$data_path)
-    data = wig_data %>%
-      generate_df(key = time)
-    
-  }
-  
-  anomalie_number = config$experiment_point_anomalies$point_anomalies_number
-  anomalie_length = config$experiment_point_anomalies$interval_anomaly_length
-  anomalie_size = ifelse(config$experiment_point_anomalies$amplitude == "small", 0.8, 0.8)  
-  
-  if( anomalie_length > 0){
-    anomalies = 
-      data %>% 
-      impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) %>%
-      impute_randomIntervalAnomaly(col = "price", length = anomalie_length, st_coeff = anomalie_size)
-  }else{
-    anomalies = 
-      data %>% 
-      impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) 
-  }
-}
+AnomalyTaskConfig = R6::R6Class("AnomalyTaskConfig", 
+  inherit = AnomalyTask,
+  public = 
+    list(
+      initialize = function(config_path) {
+          config = read_yaml(config_path)
+          data = prepare_experiment_data(config_path = config_path)
+          super$initialize(
+              id = config$experiment_point_anomalies$task_id,
+              data = data,
+              key = "price",
+              time = "time",
+              target = "label",
+              window_size = config$experiment_point_anomalies$window_size
+              )
+          }
+      )
+)
 
-prepare_experiment = function(config_path) {
-  config = read_yaml(config_path)
-  data = prepare_experiment_data(config_path = config_path)
-  experiment_task = 
-    AnomalyTask$new(
-      id = config$experiment_point_anomalies$task_id,
-      data = data,
-      key = "price",
-      time = "time",
-      target = "label",
-      window_size = config$experiment_point_anomalies$window_size
-    )
-}
 
-  
