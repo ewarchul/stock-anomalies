901123a0b0012973af4d235cafe77ceca2186c36
warbarbye <fryd.wtf@gmail.com>
2020-05-25 14:51:02 +0200

    Extend function

--- src/anomaly-experiment.R
+++ src/anomaly-experiment.R
@@ -2,15 +2,25 @@ library(tidyverse)
 library(magrittr)
 
 
-run_exp = function(models, task = NA) {
-   task_data = 
-     task$df
-   task_id = 
-     ncol(task$df)
+run_exp = function(models, task = NA, key) {
+   task_data = task$df
+   task_time = task$time_sequence
+   task_labels = task$targets
    models %>% purrr::map(function(model) {
       id = 
-        paste("model", model$model_id, "task",  
-      result =  model$predict(task_data)
+        paste("model", model$model_id, "task", task$task_id, sep = "-") 
+      predictions = 
+        model$predict(task_data)
+      vals = 
+        task_data %>%
+        dplyr::select(!!rlang::sym(key))
+      tibble::tibble(
+                     id = id,
+                     time = task_time,
+                     !!rlang::sym(key) := vals,
+                     labels = task_labels, 
+                     pred_labels = predictions
+                     )
 
   })
 }
