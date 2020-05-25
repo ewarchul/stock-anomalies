d207a531c782a6081c5f41ee451320209883e73c
warbarbye <fryd.wtf@gmail.com>
2020-05-25 13:15:58 +0200

    Extend model

--- src/anomaly-experiment.R
+++ src/anomaly-experiment.R
@@ -2,10 +2,15 @@ library(tidyverse)
 library(magrittr)
 
 
-run_exp = function(models, tasks) {
-
-    purrr::pmap(function(model, task) {
-                  print(model)
+run_exp = function(models, task = NA) {
+   task_data = 
+     task$df
+   task_id = 
+     ncol(task$df)
+   models %>% purrr::map(function(model) {
+      id = 
+        paste("model", model$model_id, "task",  
+      result =  model$predict(task_data)
 
   })
 }
--- src/anomaly-model-LOF.R
+++ src/anomaly-model-LOF.R
@@ -6,9 +6,9 @@ ModelLOF = R6::R6Class("ModelLOF",
     list(
          initialize = function(params = NULL) {
           super$initialize("LOF")
-             
             self$param_set$k = controlParam(params, "k", 10)
-             
+            self$model_id =
+              paste0(self$model_id, "-k-", self$param_set$k)
           self$model_struct = 
              purrr::partial(
                 DescTools::LOF, 
