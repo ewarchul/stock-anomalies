2fdf283cd0488e628cfa2cb3108d2886e75f47ef
warbarbye <fryd.wtf@gmail.com>
2020-05-25 11:49:04 +0200

    Fix add_lag function case when window size equals 0

--- src/utils-task.R
+++ src/utils-task.R
@@ -12,14 +12,17 @@ library(magrittr)
 #' @examples todo
 
 add_lag = function(data, key, win_size) {
-  lags = 
-    1:win_size %>%
-    purrr::map(function(lag) {
-      data %>%
-        dplyr::transmute(
-                      !!rlang::sym(paste(key, "lag", lag, sep = "_")) := dplyr::lag(!!rlang::sym(key), lag) 
-                      )
-    }) %>%
-    purrr::reduce(dplyr::bind_cols)
-  dplyr::bind_cols(data, lags)
+  if(win_size) {
+    lags = 
+      1:win_size %>%
+      purrr::map(function(lag) {
+        data %>%
+          dplyr::transmute(
+                        !!rlang::sym(paste(key, "lag", lag, sep = "_")) := dplyr::lag(!!rlang::sym(key), lag) 
+                        )
+      }) %>%
+      purrr::reduce(dplyr::bind_cols)
+    dplyr::bind_cols(data, lags)
+  } else 
+      data
 }
