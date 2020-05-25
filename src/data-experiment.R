44321cd5f6b62794795391aea8d83637778cd3e3
Aleksandra <oichii666@gmail.com>
2020-05-25 13:17:35 +0200

    function added

--- src/data-experiment.R
+++ src/data-experiment.R
@@ -4,28 +4,38 @@ source("../src/data-preparation.R")
 source("../src/data-generation.R")
 source("../src/data-visualization.R")
 
-config = read_yaml("../data/experiment.yml")
-wow_data = prepare_WoWToken("../data/history_prices_full.json")
-anomalie_number = config$experiment_point_anomalies$point_anomalies_number
-anomalie_length = config$experiment_point_anomalies$interval_anomaly_length
-anomalie_size = ifelse(config$experiment_point_anomalies$amplitude == "small", 0.4, 0.8)
-wow_eu = wow_data %>%
-  dplyr::filter(region == "eu") %>%
-  generate_df(key = time)
+#' Prepare wow toke data based on config 
+#'
+#' @description reads data, inpute anomalies 
+#' @param data_path path to data (wow token - JSON)
+#' @param config_path path to config (yaml)
 
-if( anomalie_length > 0){
-  wow_anomalies = 
-    wow_eu %>% 
-    impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) %>%
-    impute_randomIntervalAnomaly(col = "price", length = anomalie_length, st_coeff = anomalie_size)
-}else{
-  wow_anomalies = 
-    wow_eu %>% 
-    impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) 
+prepare_experiment = function(data_path, config_path) {
+  config = read_yaml(config_path)
+  wow_data = prepare_WoWToken(data_path)
+  anomalie_number = config$experiment_point_anomalies$point_anomalies_number
+  anomalie_length = config$experiment_point_anomalies$interval_anomaly_length
+  anomalie_size = ifelse(config$experiment_point_anomalies$amplitude == "small", 0.4, 0.8)
+  wow_eu = wow_data %>%
+    dplyr::filter(region == "eu") %>%
+    generate_df(key = time)
+  
+  if( anomalie_length > 0){
+    wow_anomalies = 
+      wow_eu %>% 
+      impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) %>%
+      impute_randomIntervalAnomaly(col = "price", length = anomalie_length, st_coeff = anomalie_size)
+  }else{
+    wow_anomalies = 
+      wow_eu %>% 
+      impute_randomPointAnomaly(col = "price", n = anomalie_number, st_coeff = anomalie_size) 
+  }
+
 }
 
-wow_anomalies %>%
+config_path = "../data/experiment.yml"
+data_path = "../data/history_prices_full.json"
+wow_anomalies = prepare_experiment(data_path, config_path)
+wow_anomalies %>% 
   filter(label == "TRUE")
-
 wow_anomalies %>% plot_ts()
-wow_anomalies %>% plot_ts(start_date = "2020-01-14 10:30:21", end_date = "2020-04-10 10:30:21")
