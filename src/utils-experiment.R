cebc7e452224e7e04e144f43b20663fbe47bee7f
warbarbye <fryd.wtf@gmail.com>
2020-05-27 11:01:18 +0200

    Generalize compute_metrics(function)

--- src/utils-experiment.R
+++ src/utils-experiment.R
@@ -68,7 +68,7 @@ get_metrics = function(result) {
 #' @return [Tibble]
 #' @seealso `get_metrics()`
 
-compute_metrics = function(results) {
+compute_metrics = function(results, metric_func) {
   results %>% 
     purrr::map(function(result) {
       exp_id = 
@@ -77,7 +77,7 @@ compute_metrics = function(results) {
         dplyr::slice(1)
       metrics = 
         result %>%
-        get_metrics() 
+        metric_func() 
       dplyr::bind_cols(exp_id, metrics)
   })
 }
